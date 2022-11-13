869be2d8ee0ddaa89a1dc4ec64466930cff7319a
Aleksei S <aleksioprime@gmail.com>
2022-11-13 11:10:07 +0300

    test

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const imgs = document.querySelectorAll('.images img');
+const modal = document.querySelector('.modal');
+const activeImage = document.querySelector('#active-image');
+const overlay = document.querySelector('.overlay');
+const closeIcon = document.querySelector('#close');
+
+
+imgs.forEach(img => img.addEventListener('click', displayImage)); 
+
+function displayImage(e) { 
+    // console.log(e.target);
+    activeImage.src = e.target.src;
+    console.log(modal);
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+}
+
+closeIcon.addEventListener('click', closeImage);
+
+function closeImage() {
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
+overlay.addEventListener('click', closeImage );
